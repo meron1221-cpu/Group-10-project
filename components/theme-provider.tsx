3d39b5661fd1cfa57367c437c9da1b7172c0138c
dave-zed <dawitaddis527@gmail.com>
2025-10-07 14:20:11 +0300

    Add comment explaining ThemeProvider component

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+// ThemeProvider component wraps the app with the Next.js themes context.
+// This enables dynamic light/dark mode support and custom theming across the application.
+// It simply forwards all props to the `NextThemesProvider` from the `next-themes` package.
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
